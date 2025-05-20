6b0f5506de74dbc86a04a902ec56dee3e865d1ae
mitchelllipyansky <mitchell.lipyansky@macaulay.cuny.edu>
2025-05-20 16:32:43 -0400

    fixing dependency issue

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -54,6 +54,7 @@ function Search() {
   // Apply filters when cuisine changes
   useEffect(() => {
     applyFilters();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCuisine]);
 
   return (
